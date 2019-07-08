c7c0397ec5236e5c0f2c7f82e820b6fe10936066
Nikita Mistyukov <nekifirus@gmail.com>
2019-07-08 18:59:01 +0000

    ssh and tramp

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home.packages = [
@@ -101,6 +101,8 @@
       ekpgs.yaml-mode
       ekpgs.yasnippet
       ekpgs.yasnippet-snippets
+      ekpgs.docker-tramp
+      ekpgs.counsel-tramp
     ];
   };
 
@@ -118,6 +120,26 @@
     sessionVariables = {};
     shellAliases = {};
     bashrcExtra = ''
+      case "$TERM" in
+     "dumb")
+          unsetopt zle
+          unsetopt prompt_cr
+          unsetopt prompt_subst
+          unfunction precmd
+          unfunction preexec
+          PS1="> "                # Tramp hangs #3
+          ;;
+     xterm*|rxvt*|eterm*|screen*)
+          PS1="\u@\h:\W: $  "
+          ;;
+     linux*)
+          PS1="\u@\h:\W: $  "
+          ;;
+     *)
+          PS1="> "
+          ;;
+      esac
+
       eval "$(direnv hook bash)"
     '';
   };
@@ -126,7 +148,15 @@
     enable = true;
     defaultCacheTtl = 1800;
     enableSshSupport = true;
+    sshKeys = [
+      "AB5D1B410584680A5E8B2C94A3B6EE46C295B2CD"
+      "1BAB252022782531597538185AA83E75A2428470"
+    ];
+    enableExtraSocket = true;
   };
+
+
+  programs.ssh.enable = false;
 
   programs.git = {
     enable = true;
@@ -134,6 +164,16 @@
     userEmail = "nekifirus@gmail.com";
     ignores = ["*~" "*.swp" ".direnvrc"];
   };
+
+
+  # Prevent clobbering SSH_AUTH_SOCK
+  home.sessionVariables.GSM_SKIP_SSH_AGENT_WORKAROUND = "1";
+
+  # Disable gnome-keyring ssh-agent
+  xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
+    ${lib.fileContents "${pkgs.gnome3.gnome-keyring}/etc/xdg/autostart/gnome-keyring-ssh.desktop"}
+    Hidden=true
+  '';
 
   programs.home-manager.enable = true;
   programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
