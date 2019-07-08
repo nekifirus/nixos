42ba678443d71c5dbd9df90efcc5d16e66d353db
Nikita Mistyukov <nekifirus@gmail.com>
2019-07-08 19:00:25 +0000

    improove elixir project shell file

--- mtshell.nix
+++ mtshell.nix
@@ -5,21 +5,37 @@
 # This avoids typing `pkgs.` before each package name.
 with pkgs;
 
+let
+
+  inherit (lib) optional optionals;
+
+in
+
 # Defines a shell.
 mkShell {
   # Sets the build inputs, i.e. what will be available in our
   # local environment.
-  buildInputs = [ 
+  buildInputs = [
+    git
+
     elixir
     erlang
-
-    postgresql100
-    hex2nix
 
     libtool
     autoconf
     automake
     gmp
 
- ];
+ ]
+ ++ optional stdenv.isLinux glibcLocales # To allow setting consistent locale on linux
+ ++ optional stdenv.isLinux inotify-tools # For file_system
+ ++ optional stdenv.isLinux libnotify # For ExUnit
+ ;
+
+ # Set up environment vars
+ shellHook = ''
+   export LANG="en_US.UTF-8"
+   export LC_ALL="en_US.UTF-8"
+   export PGDATA="$PWD/db"
+ '';
 }
