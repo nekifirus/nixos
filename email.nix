3e35f6a53e7094bb364da6c3681a5a753ec217fe
Nikita Mistyukov <nekifirus@gmail.com>
2020-09-12 11:12:25 +0500

    add mbsync

---
+++ email.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+{
+  programs.mbsync.enable = true;
+  programs.msmtp.enable = true;
+  programs.notmuch = {
+    enable = true;
+    hooks = {
+      preNew = "mbsync --all";
+    };
+  };
+  accounts.email = {
+    accounts.nekifirus = {
+      address = "nekifirus@gmail.com";
+      gpg = {
+        key = "nekifirus@gmail.com";
+        signByDefault = true;
+      };
+      imap.host = "imap.gmail.com";
+      mbsync = {
+        enable = true;
+        create = "maildir";
+      };
+      msmtp.enable = true;
+      notmuch.enable = true;
+      primary = true;
+      realName = "Nikita Mistyukov";
+      signature = {
+        text = ''
+        '';
+        showSignature = "append";
+      };
+      passwordCommand = "gpg2 -q -d ~/.authinfo.gpg | awk 'FNR == 2 {print $6}'";
+      smtp = {
+        host = "smtp.gmail.com";
+      };
+      userName = "nekifirus@gmail.com";
+    };
+  };
+}
