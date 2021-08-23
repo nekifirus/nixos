8e4997a12ea5c78d4981593d35dff12b5133b9b6
Nikita Mistyukov <nekifirus@gmail.com>
2021-08-24 00:43:07 +0500

    fix emacs overlay commit

--- emacs-overlay.nix
+++ emacs-overlay.nix
@@ -1,7 +1,7 @@
 {
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      url = https://github.com/nix-community/emacs-overlay/archive/abc392cf4bd06dfa7b08fece4982445f845fbbe4.tar.gz;
     }))
   ];
 }
