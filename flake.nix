e128de7d5c408aa603a6a33688ccae387edb5c49
Nikita Mistyukov <nekifirus@gmail.com>
2021-09-05 16:30:04 +0500

    try to add home-manager

--- flake.nix
+++ flake.nix
@@ -2,17 +2,44 @@
   description = "Nekifirus system";
 
   inputs = {
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=08ef0f28e3a41424b92ba1d203de64257a9fca6a";
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=08ef0f28e3a41424b92ba1d203de64257a9fca6a";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
-    home-manager.inputs.nixpkgs.follow = "nixpkgs";
-    
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager, ... }: 
+    let 
       system = "x86_64-linux";
-      modules = [  ./configuration.nix ];
+
+      pkgs = import nixpkgs {
+        inherit system;
+        
+        config = { allowUnfree = true; };
+      };
+
+      lib = nixpkgs.lib;
+      
+    in {
+      homeManagerConfigurations = {
+        nekifirus = home-manager.lib.homeManagerConfiguration {
+          inherit system pkgs;
+
+          username = "nekifirus";
+          homeDirectory = "/home/nekifirus";
+          configuration = [
+            ./home.nix
+          ];
+        };
+      };
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+        
+          modules = [
+            ./configuration.nix
+          ];
+        };
+      };
     };
-  };
 }
