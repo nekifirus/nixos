4974ffbf22399f94691ec84d02e18fdbf7ac9e60
Nikita Mistyukov <nekifirus@gmail.com>
2021-09-05 15:11:15 +0500

    more flakes

--- flake.nix
+++ flake.nix
@@ -1,5 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=08ef0f28e3a41424b92ba1d203de64257a9fca6a";
+  description = "Nekifirus system";
+
+  inputs = {
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs?rev=08ef0f28e3a41424b92ba1d203de64257a9fca6a";
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
+    home-manager.inputs.nixpkgs.follow = "nixpkgs";
+    
+  };
 
   
   outputs = { self, nixpkgs }: {
