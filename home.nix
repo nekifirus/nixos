dc01859f4e1cd5a265860347d96bba2836ed18cc
Nikita Mistyukov <nekifirus@gmail.com>
2021-09-10 22:57:55 +0500

    add alacritty

--- home.nix
+++ home.nix
@@ -50,6 +50,19 @@
           ];
   };
 
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = { x = 2; y = 2; };
+        dynamic_padding =  true;
+        decorations = "none";
+        opacity =  0.85;
+      };
+      font.size = 13;
+    };
+  };
+
   programs.bash = {
     enable = true;
     historySize = 10000;
