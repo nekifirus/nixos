05489438fe784e75c8292bdb06789f7a03d79a6a
Nikita Mistyukov <nekifirus@gmail.com>
2021-09-12 14:39:43 +0500

    add sxhkd

--- home.nix
+++ home.nix
@@ -94,6 +94,17 @@
     ];
   };
 
+  services.sxhkd = {
+    enable = true;
+    keybindings = {
+      "super + shift + p" = "passmenu";
+      "super + p" = "rofi -show combi -theme dmenu";
+      "super + a" = "emacsclient -nc";
+      "Print" = "sleep 0.5 && scrot -s '%Y-%m-%d-%H:%M:%S_$wx$h_scrot.png' -e 'mv -n $f ~/scshots/'";
+      "super + Print" = "scrot '%Y-%m-%d-%H:%M:%S_scrot.png' -e 'mv -n $f ~/scshots/'";
+    };
+  };
+
   services.gpg-agent = {
     enable = true;
     pinentryFlavor = null;
