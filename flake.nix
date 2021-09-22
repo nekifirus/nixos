622249eb4af77970e0ab50b41e07aa1bcab73656
Nikita Mistyukov <nekifirus@gmail.com>
2021-09-23 00:08:57 +0500

    reorganization

--- flake.nix
+++ flake.nix
@@ -18,33 +18,35 @@
         };
         modules = [
           home-manager.nixosModules.home-manager
-          ./nix_config.nix
-          ./configuration.nix
-          ./alacritty.nix
-          ./hardware-configuration.nix
-          ./xserver.nix
-          ./packages.nix
-          ./redshift.nix
-          ./fonts.nix
-          ./yggdrasil.nix
-          ./locale.nix
-          ./email.nix
-          ./emacs.nix
-          ./gpg.nix
-          ./tmux.nix
-          ./git.nix
-          ./rofi.nix
-          ./bash.nix
-          ./xresources.nix
-          ./syncthing.nix
-          ./direnv.nix
-          ./ssh.nix
-          ./virtualisation.nix
-          ./network_manager.nix
-          ./openvpn.nix
-          ./sound.nix
-          ./users.nix
-          ./browser.nix
+          ./nixos-host.nix
+          
+          ./system/nix_config.nix
+          ./system/packages.nix
+          ./system/openvpn.nix
+          ./system/redshift.nix
+          ./system/sound.nix
+          ./system/virtualisation.nix
+          ./system/yggdrasil.nix
+          ./system/fonts.nix
+          ./system/locale.nix
+          ./system/users.nix
+          
+          ./user/alacritty.nix
+          ./user/xserver.nix
+          ./user/email.nix
+          ./user/emacs.nix
+          ./user/gpg.nix
+          ./user/tmux.nix
+          ./user/git.nix
+          ./user/rofi.nix
+          ./user/bash.nix
+          ./user/xresources.nix
+          ./user/syncthing.nix
+          ./user/direnv.nix
+          ./user/ssh.nix
+          ./user/network_manager.nix
+          ./user/browser.nix
+          
         ];
       };
     };
