f6b6988b8558bf686e64aea86b533fc8552f11c8
Nikita Mistyukov <nekifirus@gmail.com>
2022-12-09 13:37:53 +0300

    remove unused code

--- user/emacs.nix
+++ user/emacs.nix
@@ -46,7 +46,9 @@ in
   # programs.emacs.package = pkgs.emacsUnstable;
   home-manager.users.nekifirus.programs.emacs.extraPackages = epkgs:
     with epkgs; [
+      password-store
       ledger-mode
+      flycheck-ledger
       restclient
       poetry
       org-mime
