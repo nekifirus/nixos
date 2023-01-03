34a052f6a77fc8aaffc5ae6e6cb4a0211e12380d
Nikita Mistiukov <nekifirus@gmail.com>
2023-01-03 08:24:48 +0300

    add window managers for test

--- user/xserver.nix
+++ user/xserver.nix
@@ -9,6 +9,8 @@
     xkbOptions = "grp:win_space_toggle, grp_led:caps, ctrl:swapcaps, terminate:ctr_alt_bksp";
     windowManager.awesome.enable = true;
     windowManager.stumpwm.enable = true;
+    windowManager.dwm.enable = true;
+    windowManager.exwm.enable = true;
   };
   home-manager.users.nekifirus.home.packages = with pkgs; [
     electrum
