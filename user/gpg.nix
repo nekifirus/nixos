b557150016944ae5fabd741da8b97c9ed8612beb
Nikita Mistiukov <nekifirus@gmail.com>
2026-03-22 00:54:17 +0100

    add to pinetry information - who is using gpg

--- user/gpg.nix
+++ user/gpg.nix
@@ -1,16 +1,33 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  pinentryWithNotify = pkgs.writeShellScriptBin "pinentry" ''
+    CALLER="неизвестно"
+    CALLER_PID=$(${pkgs.iproute2}/bin/ss -xp 2>/dev/null \
+      | grep 'S\.gpg-agent[^.]' \
+      | grep -v '"gpg-agent"' \
+      | grep -oP 'pid=\K[0-9]+' \
+      | head -1)
+    if [ -n "$CALLER_PID" ]; then
+      CALLER=$(ps -p "$CALLER_PID" -o comm= 2>/dev/null || echo "неизвестно")
+    fi
+    ${pkgs.libnotify}/bin/notify-send \
+      --icon=dialog-password \
+      --urgency=normal \
+      "GPG: запрос пароля" \
+      "Приложение: $CALLER"
+    exec ${pkgs.pinentry-gnome3}/bin/pinentry-gnome3 "$@"
+  '';
+in
 
 {
-  home-manager.users.nekifirus.home.packages = with pkgs; [
-    pinentry-gnome3
-  ];
   services.pcscd.enable = true;
   home-manager.users.nekifirus.programs.ssh.enable = false;
   home-manager.users.nekifirus.programs.gpg.enable = true;
   home-manager.users.nekifirus.services.gpg-agent = {
     enable = true;
-    pinentry.package = pkgs.pinentry-gnome3;
-    defaultCacheTtl = 7200;
+    pinentry.package = pinentryWithNotify;
+    defaultCacheTtl = 1200;
     enableSshSupport = true;
     sshKeys = [
       "AB5D1B410584680A5E8B2C94A3B6EE46C295B2CD"
